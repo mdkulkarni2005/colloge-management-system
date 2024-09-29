8a8f923df81add19f746d9aeb842da8a8e6c3390
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-09-29 17:03:06 +0530

    Add files via upload

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,23 @@
+import UserCard from '@/components/UserCard'
+import React from 'react'
+
+const AdminPage = () => {
+  return (
+    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+      {/* LEFT */}
+      <div className='w-full lg:w-2/3'>
+      {/* USER CARD */}
+      <div className='flex gap-4 justify-between'>
+        <UserCard type='student'/>
+        <UserCard type='teacher'/>
+        <UserCard type='parent'/>
+        <UserCard type='staff'/>
+      </div>
+      </div>
+      {/* RIGHT */}
+      <div className='w-full lg:w-1/3'>r </div>
+    </div>
+  )
+}
+
+export default AdminPage
---
+++ src/components/UserCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserCard = ({type}:{type:string}) => {
+  return (
+    <div className='rounded-2xl odd:bg-lamaPurple even:bg-lamaYellow p-4 flex-1'>
+        test
+    </div>
+  )
+}
+
+export default UserCard
