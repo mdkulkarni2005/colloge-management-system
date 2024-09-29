cd36949069308c8e5a80bfabca6a3eda8c2062a9
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-09-29 19:24:02 +0530

    stream 3 string code

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from '@/components/CountChart'
 import UserCard from '@/components/UserCard'
 import React from 'react'
 
@@ -5,14 +6,25 @@ const AdminPage = () => {
   return (
     <div className='p-4 flex gap-4 flex-col md:flex-row'>
       {/* LEFT */}
-      <div className='w-full lg:w-2/3'>
+      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
       {/* USER CARD */}
-      <div className='flex gap-4 justify-between'>
+      <div className='flex gap-4 justify-between flex-wrap'>
         <UserCard type='student'/>
         <UserCard type='teacher'/>
         <UserCard type='parent'/>
         <UserCard type='staff'/>
       </div>
+      {/* MIDDLE CHART */}
+      <div className='flex gap-4 flex-col lg:flex-row'>
+        {/* COUNT CHART */}
+        <div className='w-full lg:w-1/3 h-[450px]'>
+          <CountChart />
+        </div>
+        {/* ATTENDANCE CHART */}
+        <div className='w-full lg:w-2/3 h-[450px]'></div>
+      </div>
+      {/* BOTTOM CHART */}
+      <div className=''></div>
       </div>
       {/* RIGHT */}
       <div className='w-full lg:w-1/3'>r </div>
---
+++ src/components/AttendanceChart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AttendanceChart = () => {
+  return (
+    <div>AttendanceChart</div>
+  )
+}
+
+export default AttendanceChart
