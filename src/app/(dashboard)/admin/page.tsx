8abc80d11592df4f7450b18f318b80aa222203a9
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-09-30 17:50:13 +0530

    stream 4 code

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,35 +1,44 @@
-import CountChart from '@/components/CountChart'
-import UserCard from '@/components/UserCard'
-import React from 'react'
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+import EventCalender from "@/components/EventCalendar";
+import FinanceChart from "@/components/FinanceChart";
+import UserCard from "@/components/UserCard";
+import React from "react";
 
 const AdminPage = () => {
   return (
-    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
-      <div className='w-full lg:w-2/3 flex flex-col gap-8'>
-      {/* USER CARD */}
-      <div className='flex gap-4 justify-between flex-wrap'>
-        <UserCard type='student'/>
-        <UserCard type='teacher'/>
-        <UserCard type='parent'/>
-        <UserCard type='staff'/>
-      </div>
-      {/* MIDDLE CHART */}
-      <div className='flex gap-4 flex-col lg:flex-row'>
-        {/* COUNT CHART */}
-        <div className='w-full lg:w-1/3 h-[450px]'>
-          <CountChart />
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* USER CARD */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
         </div>
-        {/* ATTENDANCE CHART */}
-        <div className='w-full lg:w-2/3 h-[450px]'></div>
-      </div>
-      {/* BOTTOM CHART */}
-      <div className=''></div>
+        {/* MIDDLE CHART */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart />
+          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
       {/* RIGHT */}
-      <div className='w-full lg:w-1/3'>r </div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalender />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
---
+++ src/components/EventCalendar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+const events = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consecteture adipiscing elit."
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consecteture adipiscing elit."
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor",
+    time: "12:00 PM - 2:00 PM",
+    description: "Lorem ipsum dolor sit amet, consecteture adipiscing elit."
+  }
+]
+
+const EventCalender = () => {
+  const [value, onChange] = useState<Value>(new Date());
+
+  return (
+    <div className="bg-white p-4 rounded-md">
+      <Calendar onChange={onChange} value={value} />
+    </div>
+  );
+};
+
+export default EventCalender;
